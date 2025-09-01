e9db2fae20fd456c9d6361568b501f72e4df822b
KonradS <konrads733@gmail.com>
2025-09-01 22:18:41 +0200

    Add early stopping parameter to training

--- DSML/train.py
+++ DSML/train.py
@@ -5,7 +5,7 @@ import shutil
 model = YOLO("yolov5nu.pt")
 
 # Train the model with use of data augmentation
-results = model.train(data="dataset/processed/data.yaml", epochs=100, imgsz=640, device=0, freeze = 24)
+results = model.train(data="dataset/processed/data.yaml", epochs=100, imgsz=640, device=0, freeze = 24, patience = 10)
 
 # Create "my_model" folder to store model weights and train results
 os.mkdir("/models/my_model")
