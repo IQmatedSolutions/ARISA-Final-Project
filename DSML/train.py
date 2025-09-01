b6d265979af0a1ca8f92c7e7a3b8da8ce4d1804c
KonradS <konrads733@gmail.com>
2025-09-02 00:11:15 +0200

    Fix model path

--- DSML/train.py
+++ DSML/train.py
@@ -13,9 +13,9 @@ model = YOLO("yolov5nu.pt")
 results = model.train(data="dataset/processed/data.yaml", epochs=30, imgsz=640, device=0, freeze = 24)
 
 # Create "my_model" folder to store model weights and train results
-os.mkdir("/models/my_model")
-shutil.copyfile("runs/detect/train/weights/best.pt", "/models/my_model/ARISA.pt")
+os.mkdir("models/my_model")
+shutil.copyfile("runs/detect/train/weights/best.pt", "models/my_model/ARISA.pt")
 
 # Export model to ncnn version so it can be used by Raspberry Pi
-model = YOLO("/models/my_model/ARISA.pt")
+model = YOLO("models/my_model/ARISA.pt")
 model.export(format="ncnn")
