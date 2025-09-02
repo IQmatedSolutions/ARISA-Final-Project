f6d665ed9a88c0ee9ef8c147ff8bee0b3f05bbfe
KonradS <konrads733@gmail.com>
2025-09-02 11:24:33 +0200

    Add task step to export model

--- DSML/train.py
+++ DSML/train.py
@@ -1,8 +1,5 @@
 from ultralytics import YOLO
 from ultralytics import settings
-import os
-import shutil
-
 
 # Update a setting
 settings.update({"mlflow": True})
@@ -11,10 +8,3 @@ model = YOLO("yolov5nu.pt")
 
 # Train the model with use of data augmentation
 results = model.train(data="dataset/processed/data.yaml", epochs=2, imgsz=640, device=0, freeze = 24)
-
-# Create "my_model" folder to store model weights and train results
-shutil.copyfile("runs/detect/train/weights/best.pt", "models/my_model/ARISA.pt")
-
-# Export model to ncnn version so it can be used by Raspberry Pi
-model = YOLO("models/my_model/ARISA.pt")
-model.export(format="ncnn")
