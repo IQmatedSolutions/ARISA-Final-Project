eff430be01c9ccc7a24193b2f571eee86e0e26ca
KonradS <konrads733@gmail.com>
2025-09-02 11:07:28 +0200

    Add directory creation for models

--- DSML/train.py
+++ DSML/train.py
@@ -10,10 +10,9 @@ settings.update({"mlflow": True})
 model = YOLO("yolov5nu.pt")
 
 # Train the model with use of data augmentation
-results = model.train(data="dataset/processed/data.yaml", epochs=30, imgsz=640, device=0, freeze = 24)
+results = model.train(data="dataset/processed/data.yaml", epochs=2, imgsz=640, device=0, freeze = 24)
 
 # Create "my_model" folder to store model weights and train results
-os.mkdir("models/my_model")
 shutil.copyfile("runs/detect/train/weights/best.pt", "models/my_model/ARISA.pt")
 
 # Export model to ncnn version so it can be used by Raspberry Pi
