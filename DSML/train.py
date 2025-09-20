55ba486eaf4031f73667aa10a90ee37bb1f96147
Konrad_S <114168278+KonradSdev@users.noreply.github.com>
2025-09-20 11:14:24 +0200

    Update train.py

--- DSML/train.py
+++ DSML/train.py
@@ -7,4 +7,4 @@ settings.update({"mlflow": True})
 model = YOLO("yolov5nu.pt")
 
 # Train the model with use of data augmentation
-results = model.train(data="dataset/processed/data.yaml", epochs=30, imgsz=640, device=0, freeze = 24)
+results = model.train(data="dataset/processed/data.yaml", epochs=30, imgsz=640, device=0, freeze = 10)
